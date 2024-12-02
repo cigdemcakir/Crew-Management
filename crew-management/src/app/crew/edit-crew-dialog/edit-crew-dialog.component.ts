8f0d190290c54923ed9d5603b1ce6caf7651920b
Çiğdem Çakır <cigdemcakir4897@gmail.com>
2024-12-02 11:59:18 +0300

    edited edit-crew-dialog component

--- crew-management/src/app/crew/edit-crew-dialog/edit-crew-dialog.component.ts
+++ crew-management/src/app/crew/edit-crew-dialog/edit-crew-dialog.component.ts
@@ -7,6 +7,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-edit-crew-dialog',
@@ -15,7 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatSelectModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule,  CommonModule, FormsModule, ReactiveFormsModule],
+    MatButtonModule,  CommonModule, FormsModule, ReactiveFormsModule,MatIconModule],
   templateUrl: './edit-crew-dialog.component.html',
   styleUrls: ['./edit-crew-dialog.component.css'],
 })
